dad6d46d68aa24f1da31ffcfb99b8fce2f479385
subinkim32 <salgeum1222@gmail.com>
2023-10-12 17:07:35 +0900

    05

--- main.c
+++ main.c
@@ -3,14 +3,44 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int square(int a)
+int factorial(n)
 {
-	return(a*a);
+	int res = 1;
+	int i;
+	for ( i = 1 ; i <= n ; i++ )
+		res *= i;
+	
+	return res;
+}
+
+int combination(int n, int r)
+{
+	int boonmo, boonja;
+	
+	boonja = factorial(n);
+	boonmo = factorial(n-r)*factorial(r);
+	
+	return (boonja/boonmo);
+	
 }
 
 int main(int argc, char *argv[])
 {
-	int a = 2;
-	a = square(a);
-	printf("a = %d\n", a);
+	int n, r;
+	int result;
+	
+	// 1. input n and r
+	printf("input n: \n");
+	scanf("%d", &n);
+	
+	printf("input r: \n");
+	scanf("%d", &r);
+	
+
+	// 2. calculate each boonmo, boonja and final result
+	result = combination(n, r);
+	
+	// 3. print it
+	printf("result is %d.\n", result);
+
 }
