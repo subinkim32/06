1b4ead762c675d89040b34c033cb9e3e3f76d272
subinkim32 <salgeum1222@gmail.com>
2023-10-12 15:43:18 +0900

    02

--- main.c
+++ main.c
@@ -3,18 +3,16 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void print_star()
+void func1()
 {
-	int i;
-	
-	for ( i = 0 ; i < 10 ; i++ )
-		printf("*");
+	int x;
+	printf("func1 x is at %p\n", &x);
 }
 
 int main(void) {
-	print_star();
-	print_star();
-	print_star();
+	int x;
+	printf("main x is at %p\n", &x);
+	func1();
 			
 	return 0;
 }
